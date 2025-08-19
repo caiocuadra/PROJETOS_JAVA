332ab26c208abb3113064a268b2c0e7b3eac0e25
caiocuadra <137195296+caiocuadra@users.noreply.github.com>
2025-08-19 00:04:49 -0300

    Create Missao2_DESAFIO3.java

---
+++ MISSAO 2 - DESAFIO 3 - ESTRUTURA DE CONTROLE DE REPETIÇÃO/Missao2_DESAFIO3.java
@@ -0,0 +1,35 @@
+package Missao2;
+/*
+ * Autor: Caio Henrique Mota Cuadra
+ * Data: 18/08/2025
+ */
+
+//INICIO DO PROGRAMA
+public class Missao2_DESAFIO3 
+{// inicio da classe principal
+
+	public static void main(String[] args) 
+	{//inicio do metodo principal
+		
+		//inicio do bloco 1 e titulo
+		System.out.println("MISSÃO 2 - DESAFIO 3 - WHILE");
+		System.out.println("Código exemplo de Laço de Repetição");
+		//fim do bloco 1
+		
+		//declara o valor da variavel idade
+		int idade = 15;
+		
+		//inicio do bloco 2 (laço de repetição while)
+		while (idade < 18) {
+			System.out.println(idade);
+			idade = idade + 1;	
+		}
+		//fim do bloco 2 (laço de repetição while)
+		
+		System.out.println("Fim do código do laço de repetição while");
+		System.out.println("MISSÃO 2 - DESAFIO 3 - concluído!");
+
+	}//fim do metodo principal
+
+}//fim da classe principal
+//FIM DO PROGRAMA
