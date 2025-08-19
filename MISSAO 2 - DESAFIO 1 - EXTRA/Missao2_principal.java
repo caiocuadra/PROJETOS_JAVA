0a76e149e826678ce615439d9fe3f42b1bb875b1
caiocuadra <137195296+caiocuadra@users.noreply.github.com>
2025-08-18 21:55:57 -0300

    Create Missao2_principal.java

---
+++ MISSAO 2 - DESAFIO 1 - EXTRA/Missao2_principal.java
@@ -0,0 +1,33 @@
+package Missao2;
+/*
+ * Autor: Caio Henrique Mota Cuadra
+ * Data: 18/08/2025
+ */
+
+//INICIO DO PROGRAMA
+public class Missao2_principal 
+{// inicio classe principal
+
+	public static void main(String[] args) 
+	{//inicio metodo principal
+		
+		//inicio do bloco 1
+		//apresenta o título da Missão:
+		System.out.println("Início da Missão 2");
+		//declara e atribui valor a variável idade
+		int idade = 40;
+		System.out.println("Programa para testar a idade do aluno.");
+	//fim bloco 1
+		//realiza o teste da condição
+		if(idade == 40) {
+			//bloco 2
+			System.out.println("O aluno tem exatamente 40 anos");
+		}
+		//bloco 3
+		System.out.println("Fim do teste de idade.");
+		
+
+	}//fim metodo principal
+
+}// fim classe principal
+//FIM DO PROGRAMA
