55ed5f4bac35fe1ada35278bb0739179d4499de6
caiocuadra <137195296+caiocuadra@users.noreply.github.com>
2025-08-19 00:29:26 -0300

    Create Missao2_DESAFIO5.java

---
+++ MISSAO 2 - DESAFIO 5 - ESTRUTURA DE CONTROLE DE REPETIÇÃO/Missao2_DESAFIO5.java
@@ -0,0 +1,24 @@
+package Missao2;
+/*
+ * Autor: Caio Henrique Mota Cuadra
+ * Data: 19/08/2025
+ */
+//INICIO DO PROGRAMA
+public class Missao2_DESAFIO5 
+{// inicio da classe principal
+	public static void main(String args[]) 
+	{// inicio do metodo principal
+		
+		System.out.println("MISSÃO 2 - DESAFIO 5 - FOR");
+		System.out.println("Mostra os números de 0 a 9.");
+		
+		//inicio do laço de repetição for
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("FIM DO PROGRAMA");
+		
+	}// fim do metodo principal
+	
+}// fim da classe principal
+//FIM DO PROGRAMA
